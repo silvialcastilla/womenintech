9e0f8efd79a8a9e59b199720d50f4946ddc9dc0c
silvialcastilla <silope0302@gmail.com>
2021-03-03 17:06:06 +0100

    pie with changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Pie } from "react-chartjs-2";
 
-
-
+const arraydePrueba = {
+  labels: ["Mujeres", "Hombres"],
+  data: [53, 40],
+};
+const state = {
+  labels: arraydePrueba.labels,
+  datasets: [
+    {
+      label: "Rainfall",
+      backgroundColor: ["#000000", "#FF0000"],
+      hoverBackgroundColor: ["#FFFF00", "#4B5000"],
+      data: arraydePrueba.data,
+    },
+  ],
+};
 
 function ArchivePage() {
   return (
     <div className="archive-page">
       <h1>HomePage</h1>
+      <Pie
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: "La comparativa",
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
     </div>
   );
 }
