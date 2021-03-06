7b2ca2a39bf0d353487aaf5ba0acd7f5928c78b3
silvialcastilla <silope0302@gmail.com>
2021-03-06 14:22:02 +0100

    about us finished

--- src/components/Target/Target.jsx
+++ src/components/Target/Target.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import "./Target.css"
+import React, { useState } from "react";
+import "./Target.css";
 
-function Target({data}) {
-    console.log(data)
+function Target({ data }) {
+  // const [teachers, setTeachers] = useState[
+  //   (Clara, Miriam, Nora, Sol, Silvia, Marina, Marinaa, Reyes)
+  // ];
+
   return (
     <div className="target-container">
-        <div className="target-little-container">
-            <img></img>
-            <p>{data.name}</p>
-            <p>{data.profession}</p>
-            <p>{data.description}</p>
-        </div>
+      <div className="target-little-container">
+        <img className="target-img" src={data.img} alt={data.name}></img>
+        <p className="titles-about">{data.name}</p>
+        <p className="profesion">{data.profession}</p>
+        <p className="quote">{data.description}</p>
+      </div>
     </div>
   );
 }
